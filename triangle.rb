1aeb3bf688bf40953bd20755216b6754665882d0
Reid Paape <wrpaape@gmail.com>
2015-05-21 08:59:29 -0500

    week 3 day 4 hw

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  [a, b, c].each { |side| raise TriangleError if side <= 0 }
+  unless a + b > c && a + c > b && b + c > a
+    raise TriangleError
+  end
+
+  return :equilateral if is_equ?(a, b, c)
+  return :isosceles if is_iso?(a, b, c)
+  :scalene
+end
+
+def is_equ?(a, b, c)
+  a == b && a == c ? true : false
+end
+
+def is_iso?(a, b, c)
+  a == b || a == c || b == c ? true : false
 end
 
 # Error class used in part 2.  No need to change this code.
